6b4e483db8c776421ea1d067d2e7c8b8cc410ba4
Elizabeth Dolgaya <lisalastauka@gmail.com>
2023-05-23 16:02:19 +0200

    remove comments

--- jest.config.js
+++ jest.config.js
@@ -6,9 +6,5 @@ module.exports = {
   testRegex: '(/__tests__/.*|(\\.|/)(test|spec))\\.tsx?$',
   moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
   testEnvironment: 'jsdom',
-  // setupFilesAfterEnv: ['<rootDir>/__tests__/setupTest.ts'],
   setupFilesAfterEnv: ['@testing-library/jest-dom/extend-expect'],
-  // moduleNameMapper: {
-  //   '\\.(css|less)$': 'identity-obj-proxy',
-  // },
 };
